21fb71733bf91d95dc04d2b0badaf616a3bead50
ilyass <ilyass.bentalha2@gmail.com>
2025-12-01 18:17:07 +0100

    little modification in class produit

---
+++ src/models/ProductStatus.java
@@ -0,0 +1,7 @@
+package models;
+
+public enum ProductStatus {
+    AVAILABLE,
+    OUT_OF_STOCK,
+    DISCONTINUED
+}
--- src/models/Produit.java
+++ src/models/Produit.java
@@ -1,21 +1,28 @@
 package models;
 
+
 abstract class Produit {
     protected int id;
     protected String nom;
     protected double prix;
+    protected String description;
+    protected ProductStatus status; // ProductStatus enum
     protected int quantite;
     protected int categorieId;
     protected int fournisseurID;
 
-    public Produit(int id, String nom, double prix, int quantite, int categorieId, int fournisseurID) {
+
+    public Produit(int id, String nom, double prix, int quantite, int categorieId, int fournisseurID, String description) {
         this.id = id;
         this.nom = nom;
         this.prix = prix;
         this.quantite = quantite;
         this.categorieId = categorieId;
         this.fournisseurID = fournisseurID;
+        this.description = description;
+        this.status = ProductStatus.AVAILABLE; // by default
     }
+
 
     // Methode abstraite
     public abstract String getType();
@@ -27,7 +34,9 @@ abstract class Produit {
     public int getQuantite() { return quantite; }
     public int getCategorieId() { return categorieId; }
     public int getFournisseurId() { return fournisseurID; }
+    public ProductStatus getStatus() { return status;}
 
+    public void setStatus(ProductStatus status) { this.status = status;}
     public void setQuantite(int quantite) { this.quantite = quantite; }
     public void setPrix(double prix) { this.prix = prix; }
 
